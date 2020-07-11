7bf4939e0ff76dce759b43a40fb413ee93f2eab9
ranguli <hello@joshmurphy.ca>
2020-07-11 15:40:58 -0230

    Use arrow functions and fix code styling.
    - Arrow functions are now used instead of passing functions
    - Code style issues addressed with eslint config

--- simplify.js
+++ simplify.js
@@ -1,68 +1,69 @@
 function unclutter() {
-    clutter = [
-        ".premium-banner-outer",
-        ".footer-parent",
-        ".sidecontentbox",
-        ".rank",
-        ".share",
-        ".post-crosspost-button",
-        ".midcol",
-        ".infobar.listingsignupbar",
-        ".redesign-beta-optin",
-        ".default-header",
-        ".create",
-        ".users-online",
-        ".hover.redditname",
-        ".subscribers",
-        ".account-activity-box",
-        ".debuginfo",
-        ".give-gold-button",
-        ".report-button",
-        ".linkinfo",
-        ".trending-subreddits",
-        "#header-img-a",
-        ".bottom",
-        "#sr-header-area",
-        ".help-toggle",
-        ".reddiquette",
-        ".panestack-title"
-    ]
+  const clutter = [
+    '.premium-banner-outer',
+    '.footer-parent',
+    '.sidecontentbox',
+    '.rank',
+    '.share',
+    '.post-crosspost-button',
+    '.midcol',
+    '.infobar.listingsignupbar',
+    '.redesign-beta-optin',
+    '.default-header',
+    '.create',
+    '.users-online',
+    '.hover.redditname',
+    '.subscribers',
+    '.account-activity-box',
+    '.debuginfo',
+    '.give-gold-button',
+    '.report-button',
+    '.linkinfo',
+    '.trending-subreddits',
+    '#header-img-a',
+    '.bottom',
+    '#sr-header-area',
+    '.help-toggle',
+    '.reddiquette',
+    '.panestack-title',
+  ];
 
-    clutter.forEach(function (item, index) {
-        document.querySelectorAll(item).forEach(function(a) {
-            a.remove()
-        })
+  clutter.forEach((item) => {
+    document.querySelectorAll(item).forEach((a) => {
+      a.remove();
     });
+  });
 }
 
-function clean_header_bottom_left() {
-    useless_tabs = ["controversial", "rising", "gilded"]
-    header_bottom_left = document.getElementById("header-bottom-left")
-    tab_menu = header_bottom_left.querySelector(".tabmenu");
+function cleanHeaderBottomLeft() {
+  const uselessTabs = ['controversial', 'rising', 'gilded'];
+  const headerBottomLeft = document.getElementById('header-bottom-left');
+  const tabMenu = headerBottomLeft.querySelector('.tabmenu');
 
-    tabs = tab_menu.querySelectorAll(".choice");
+  const tabs = tabMenu.querySelectorAll('.choice');
 
-    tabs.forEach(function(tab) {
-        console.log(tab.textContent);
-        if (useless_tabs.includes(tab.textContent)) {
-            tab.parentElement.remove();
-        }
-    });
+  tabs.forEach((tab) => {
+    if (uselessTabs.includes(tab.textContent)) {
+      tab.parentElement.remove();
+    }
+  });
 }
 
-function clean_header_bottom_right() {
-    header_bottom_right = document.getElementById("header-bottom-right");
+function cleanHeaderBottomRight() {
+  const headerBottomRight = document.getElementById('header-bottom-right');
+  const chatSeparator = document.getElementById('header-bottom-right').querySelectorAll('.separator')[1];
+  const chat = headerBottomRight.querySelector('#chat');
 
-    chat_separator = document.getElementById("header-bottom-right").querySelectorAll(".separator")[1];
-    header_bottom_right.querySelector("#chat").remove();
-    chat_separator.remove();
+  if (chat !== null) {
+    chat.remove();
+    chatSeparator.remove();
+  }
 }
-
 
 function simplify() {
-    unclutter();
-    clean_header_bottom_right();
-    clean_header_bottom_left();
+  unclutter();
+  cleanHeaderBottomRight();
+  cleanHeaderBottomLeft();
 }
 
 simplify();
